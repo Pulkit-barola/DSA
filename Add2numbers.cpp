79a071be16072dc0b8458e0eb15181f08c08b5c6
Pulkit barola <pulkitbarola20@gmail.com>
2024-11-17 18:23:43 +0530

    create a sum of any two number by input function

---
+++ Add2numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter the first number :- "<<endl;
+    cin>>a;
+    cout<<"Enter any second number :- "<<endl;
+    cin>>b;
+
+    c = a+b;
+    cout<<"The sum is :- "<<c<<endl;
+}
