c000f983dccfc45dbc98203fa2fc402e8e4ca68f
Pulkit barola <pulkitbarola20@gmail.com>
2024-11-17 19:21:23 +0530

    make a grading system by if else

---
+++ if-else/passorfail.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A;
+    cout<<"Enter the marks";
+    cin>>A;
+    if (A>=33)
+    {
+        cout<<"Pass!!!"<<endl;
+    }
+    else{
+        cout<<"Fail!!!";
+    }
+    
+}
