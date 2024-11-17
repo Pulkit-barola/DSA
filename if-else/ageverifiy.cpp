100b1af58dc4e5c94d28af76eacf4b20749ae22b
Pulkit barola <pulkitbarola20@gmail.com>
2024-11-17 18:42:27 +0530

    Make code for check  voting  by loop

---
+++ if-else/ageverifiy.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the age of person"<<endl;
+    cin>>a;
+    if (a>=18)
+    {
+        cout<<"You are right to vote !!!";
+    }
+    else{
+        cout<<"You have no right to vote !!!";
+    }
+    
+}
