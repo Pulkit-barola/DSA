fec49fb35f9228eced38fa81bbe3b4882511d614
Pulkit barola <pulkitbarola20@gmail.com>
2024-11-18 20:56:02 +0530

    CREATE A SQUARA PROGRAM BY FOR LOOP

---
+++ Loops/Squara_any_number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter any final number  for squaring ";
+    cin>>a;
+
+    for (int i = 1; i <=a; i++)
+    {
+        cout<<i*i<<endl;
+    }
+    
+}
